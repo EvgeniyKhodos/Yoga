47c6fb956331a1c5cfc19889da929b0faf5a1108
Evgeniy <en-hodos@mail.ru>
2020-05-09 22:49:48 +0700

    Adds Modal window

--- js/script.js
+++ js/script.js
@@ -32,3 +32,70 @@
             }
         }
     });
+
+    let deadLine = '2020-06-08';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor(t / (1000 * 60) % 60),
+            hours = Math.floor(t / (1000 * 60 * 60));
+
+        return {
+            'total': t,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours
+        }
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            seconds = document.querySelector('.seconds'),
+            minutes = document.querySelector('.minutes'),
+            hours = document.querySelector('.hours'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num) {
+                if (num < 10) {
+                    num = '0' + num;
+                }
+                return num;
+            };
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadLine);
+
+    // Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
